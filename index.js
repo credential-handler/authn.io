82e18ec187d5ceac0bdd455fd6722d7e768543ee
Hassan Almas <halmas@digitalbazaar.com>
2015-04-13 15:09:17 -0400

    Add inital REST API and DID creation.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var bedrock = require('bedrock');
 var path = require("path");
+var database = require('bedrock-mongodb');
 
 require('bedrock-express');
 require('bedrock-requirejs');
 require('bedrock-server');
 require('bedrock-views');
+
+// custom configuration
+bedrock.config.mongodb.name = 'login_hub_dev'; // default: bedrock_dev
+bedrock.config.mongodb.host = 'localhost';      // default: localhost
+bedrock.config.mongodb.port = 27017;            // default: 27017
+bedrock.config.mongodb.username = 'login_hub'; // default: bedrock
+bedrock.config.mongodb.password = 'password';   // default: password
+
+// Bower Front End Configurations
 
 bedrock.config.views.paths.push(
   path.join(__dirname)
@@ -22,6 +32,43 @@ bedrock.config.requirejs.bower.packages.push({
     }
   }
 });
+
+// Database and routes
+
+// On MongoDb being ready
+bedrock.events.on('bedrock-mongodb.ready', function(callback) {
+  database.openCollections(['CHT'], function(err) {
+    if(err) {
+      return callback(err);
+    }
+    callback();
+  });
+});
+
+bedrock.events.on('bedrock-express.configure.routes', function(app) {
+  app.get('/users/:name', function(req, res){
+    database.collections.people.insert([{name: req.param("name")}],
+     function(err, result){
+      res.send(result.result);
+    });
+  });
+
+  app.post('/DIDquery', function(req, res){
+    console.log(req.body);
+    database.collections.CHT.find({hash: req.body.hashQuery})
+    .toArray(function(err, docs){
+      if(docs.length == 0){
+        var userDID = "did:" + bedrock.util.uuid();
+        database.collections.CHT.insert([{hash: req.body.hashQuery, did: userDID}]);
+        res.send(userDID);
+      }
+      else{
+        res.send(docs[0].did);
+      }
+    });
+  });
+});
+
 
 
 bedrock.start();
