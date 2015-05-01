e339cac2610217de7b78fd38d986e9e13f13ad59
Manu Sporny <msporny@digitalbazaar.com>
2015-05-01 17:08:07 -0400

    Add test config.
    Refs #1.

---
+++ configs/test.js
@@ -0,0 +1,51 @@
+/*
+ * Loginhub test configuration.
+ *
+ * Copyright (c) 2015 The Open Payments Foundation. All rights reserved.
+ */
+var config = require('bedrock').config;
+
+// location of logs
+var _logdir = '/tmp/loginhub';
+
+// 0 means use # of cpus
+config.core.workers = 0;
+config.core.restartWorkers = true;
+
+// logging
+config.loggers.logdir = _logdir;
+config.loggers.app.filename = _logdir + '/loginhub-test-app.log';
+config.loggers.access.filename = _logdir + '/loginhub-test-access.log';
+config.loggers.error.filename = _logdir + '/loginhub-test-error.log';
+
+// only log critical errors by default
+config.loggers.console.level = 'critical';
+
+// server info
+config.server.port = 34443;
+config.server.httpPort = 34080;
+config.server.host = 'loginhub.dev:34443';
+config.server.baseUri = 'https://' + config.server.host;
+
+// mongodb config
+config.mongodb.name = 'loginhub_test';
+config.mongodb.local.collection = 'loginhub_test';
+
+// mail config
+config.mail.vars.baseUri = config.server.baseUri;
+config.mail.vars.subject = {
+  prefix: '[LOGINHUB TEST] ',
+  identityPrefix: '[LOGINHUB TEST] '
+};
+config.mail.vars.service = {
+  name: 'Loginhub Dev Test',
+  host: config.server.host
+};
+
+// views
+// branding
+config.views.brand.name = 'Loginhub Test';
+// update view vars
+config.views.vars.baseUri = config.server.baseUri;
+config.views.vars.title = config.views.brand.name;
+config.views.vars.siteTitle = config.views.brand.name;
