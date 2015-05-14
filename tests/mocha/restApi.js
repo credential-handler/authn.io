8f354f930103632dd833637fb3709086d7da469f
Manu Sporny <msporny@digitalbazaar.com>
2015-05-14 15:37:08 -0400

    Migrate to did-io.

--- tests/mocha/restApi.js
+++ tests/mocha/restApi.js
@@ -6,59 +6,10 @@ var config = require('bedrock').config;
 var forge = require('node-forge');
 var request = require('request');
 var uuid = require('node-uuid');
+var didio = require('did-io')();
 
 // base URL for tests
 var base = config.server.baseUri;
-
-// generates a unique DID
-var generateDid = function() {
-  var buffer = new Array(16);
-  uuid.v4(null, buffer, 0);
-  return 'did:' + uuid.unparse(buffer);
-};
-
-// generates a login hash given an email address and passphrase
-var generateLoginHash = function(email, passphrase) {
-  var md = forge.md.sha256.create();
-  md.update(email + passphrase);
-  return md.digest().toHex();
-};
-
-// encrypts a DID given a password
-var encryptDid = function(did, password) {
-  var pwKeyHashMethod = 'PKCS5';
-  var encryptionMethod = 'AES-GCM';
-  var salt = forge.random.getBytesSync(128);
-  var numIterations = 5;
-
-  var key = forge.pkcs5.pbkdf2(password, salt, numIterations, 16);
-
-  var iv = forge.random.getBytesSync(16);
-  var cipher = forge.cipher.createCipher('AES-GCM', key);
-
-  cipher.start({
-    iv: iv, // should be a 12-byte binary-encoded string or byte buffer
-    tagLength: 128 // optional, defaults to 128 bits
-  });
-  cipher.update(forge.util.createBuffer(did));
-  cipher.finish();
-  var encrypted = forge.util.encode64(cipher.output.getBytes());
-  var tag = forge.util.encode64(cipher.mode.tag.getBytes());
-  var iv64 = forge.util.encode64(iv);
-
-  var edid = {
-    pwKeyHashMethod: pwKeyHashMethod,
-    numIterations: numIterations,
-    salt: salt,
-    encryptionMethod: encryptionMethod,
-    authTag: tag,
-    key: key,
-    iv: iv64,
-    encrypted: encrypted
-  };
-
-  return edid;
-}
 
 describe('loginhub - REST API', function() {
   describe('/dids', function() {
@@ -66,11 +17,11 @@ describe('loginhub - REST API', function() {
     var lhRequest = request.defaults({strictSSL: false});
 
     it('should support DID document creation', function(done) {
-      var did = generateDid();
+      var did = didio.generateDid();
       var email = did + '@loginhub.dev';
       var passphrase = did + 'password';
-      var loginHash = generateLoginHash(email, passphrase);
-      var encryptedDid = encryptDid(did, passphrase);
+      var loginHash = didio.generateHash(email, passphrase);
+      var encryptedDid = didio.encrypt(did, passphrase);
       var keypair = forge.pki.rsa.generateKeyPair({bits: 512, e: 0x10001});
       var didDocument = {
         publicKeys: [keypair.publicKey],
