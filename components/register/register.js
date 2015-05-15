057953421cea2d5bbe3b4229a4c44cf9fcc7572d
Manu Sporny <msporny@digitalbazaar.com>
2015-05-15 16:42:05 -0400

    Refactor frontend registration code.

--- components/register/register.js
+++ components/register/register.js
@@ -24,10 +24,11 @@ module.controller('RegisterController', function(
     DataService.set('callback', config.data.callback);
   }
 
-  self.passwordConfirmation = '';
-  self.password = '';
+  self.passphraseConfirmation = '';
+  self.passphrase = '';
   self.username = '';
-    console.log('DataService.get(idp)', DataService.get('idpInfo'));
+  self.registering = false;
+  self.generating = false;
 
   if(!DataService.get('idpInfo')) {
     DataService.redirect('/register/idp-error');
@@ -35,37 +36,34 @@ module.controller('RegisterController', function(
 
   self.register = function() {
     // TODO: Add more validation checks
-    if(self.password != self.passwordConfirmation) {
-      return brAlertService.add('error', 'Passwords don\'t match');
+    if(self.passphrase != self.passphraseConfirmation) {
+      return brAlertService.add('error',
+        'The passphrases you entered do not match.');
     }
     if(self.username.length == 0) {
-      return brAlertService.add('error', 'Enter a username');
+      return brAlertService.add('error',
+        'You failed to provide an email address');
     }
-
-    var hash = didio.generateHash(self.username, self.password);
     var idpInfo = DataService.get('idpInfo');
+
+    // generate the private key
+    self.generating = true;
     var rsa = forge.pki.rsa;
-
-    console.log('start key generation ');
-
-    $('.container').wrapInner('<div class="spinner"></div>');
-
-    // TODO: Put spinner while key is generating
     var keypair = rsa.generateKeyPair({bits: 2048, e: 0x10001});
 
-    // places private key in browsers local storage
+    // store private key in browser local storage
+    // TODO: Encrypt the private key
     localStorage.setItem(hash, JSON.stringify(keypair.privateKey));
     // to retrieve the private key, do the following
     // var privateKey = localStorage.getItem(hash)
 
+    // generate the DID and encrypted DID data
+    var hash = didio.generateHash(self.username, self.passphrase);
     var did = didio.generateDid();
+    var encryptedDid = didio.encrypt(did, self.passphrase);
+    self.generating = true;
 
-    var encryptedDid = didio.encrypt(did, self.password);
-    console.log('Final encrypted did', encryptedDid);
-
-    console.log('end key generation');
-
-    // stores the hash to encryptedDid mapping
+    // store the hash to encryptedDid mapping
     var mappingData = {
       '@context': 'https://w3id.org/identity/v1',
       id: 'urn:sha256:' + hash,
@@ -73,57 +71,32 @@ module.controller('RegisterController', function(
     };
     Promise.resolve($http.post('/mappings/', mappingData))
       .then(function(response) {
-        console.log(response);
-
-        // TODO: user string error types not messages for comparison
-        if(response.data === 'Failed to create user') {
-          brAlertService.add(
-            'error', 'Could not create account. Use a different login/pw.');
-        } else {
-          console.log("Success");
-          console.log("idpInfo", DataService.get('idpInfo'));
-          DataService.redirect(DataService.get('idpInfo').url);
-          $scope.$apply();
+        if(response.status !== 201) {
+          throw response;
         }
-    }).then(function() {
-      // stores the DID document
-      var didDocument = {
-        '@context': 'https://w3id.org/identity/v1',
-        id: did,
-        idp: idpInfo,
-        publicKeys: [forge.pki.publicKeyToPem(keypair.publicKey)]
-      };
-      Promise.resolve($http.post('/dids/', didDocument))
-        .then(function(response) {
-          console.log(response);
-          // TODO: user string error types not messages for comparison
-          if(response.data === 'Failed to create user') {
-            brAlertService.add(
-              'error', 'Could not create account. Use a different login/pw.');
-          } else {
-            console.log("Success");
-            console.log("idpInfo", DataService.get('idpInfo'));
+      }).then(function() {
+        // store the DID document
+        var didDocument = {
+          '@context': 'https://w3id.org/identity/v1',
+          id: did,
+          idp: idpInfo,
+          publicKeys: [forge.pki.publicKeyToPem(keypair.publicKey)]
+        };
+        return Promise.resolve($http.post('/dids/', didDocument))
+          .then(function(response) {
+            if(response.status !== 201) {
+              throw response;
+            }
             DataService.redirect(DataService.get('idpInfo').url);
             $scope.$apply();
-          }
-        });
-    });
-
-    /*
-      generate public/private key
-      generate DID
-      create DID document (clientside)
-        public key
-        DID
-        IdP
-      rest api call to loginhub to create entry in db -> loginhub.com/storeDID
-        server checks work, validity, writes to db
-        {hash: DID}
-        {DID: DID Document}
-
-  // password encrypted blob / salt / encryption method / number of iterations /
-
-    */
+          });
+      }).catch(function(err) {
+        console.error('Failed to register with the network', err);
+        brAlertService.add('error',
+          'Failed to register with the network. Try a different email ' +
+          'address and passphrase');
+        self.registering = false;
+      });
   };
 });
 
