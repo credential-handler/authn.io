d43ea448d7ab63bee799f9bff4c63777e586b83b
Manu Sporny <msporny@digitalbazaar.com>
2015-05-19 15:01:16 -0400

    Migrate to did-io for frontend DID crypto.

--- components/register/register.js
+++ components/register/register.js
@@ -49,54 +49,77 @@ module.controller('RegisterController', function(
     // generate the private key
     self.generating = true;
     var rsa = forge.pki.rsa;
-    var keypair = rsa.generateKeyPair({bits: 2048, e: 0x10001});
-
-    // store private key in browser local storage
-    // TODO: Encrypt the private key
-    localStorage.setItem(hash, JSON.stringify(keypair.privateKey));
-    // to retrieve the private key, do the following
-    // var privateKey = localStorage.getItem(hash)
-
-    // generate the DID and encrypted DID data
-    var hash = didio.generateHash(self.username, self.passphrase);
-    var did = didio.generateDid();
-    var encryptedDid = didio.encrypt(did, self.passphrase);
-    self.generating = true;
-
-    // store the hash to encryptedDid mapping
-    var mappingData = {
-      '@context': 'https://w3id.org/identity/v1',
-      id: 'urn:sha256:' + hash,
-      cipherData: encryptedDid
-    };
-    Promise.resolve($http.post('/mappings/', mappingData))
-      .then(function(response) {
-        if(response.status !== 201) {
-          throw response;
+    var keypair = null;
+    var did = null;
+    new Promise(function(resolve, reject) {
+      self.generating = true;
+      rsa.generateKeyPair({
+        bits: 2048,
+        workerScript: '/bower-components/forge/js/prime.worker.js'
+      }, function(err, keypair) {
+        if(err) {
+          return reject(err);
         }
-      }).then(function() {
-        // store the DID document
-        var didDocument = {
-          '@context': 'https://w3id.org/identity/v1',
-          id: did,
-          idp: idpInfo,
-          publicKeys: [forge.pki.publicKeyToPem(keypair.publicKey)]
-        };
-        return Promise.resolve($http.post('/dids/', didDocument))
-          .then(function(response) {
-            if(response.status !== 201) {
-              throw response;
-            }
-            DataService.redirect(DataService.get('idpInfo').url);
-            $scope.$apply();
-          });
-      }).catch(function(err) {
-        console.error('Failed to register with the network', err);
-        brAlertService.add('error',
-          'Failed to register with the network. Try a different email ' +
-          'address and passphrase');
-        self.registering = false;
+        resolve(keypair);
       });
+    }).then(function(kp) {
+      keypair = kp;
+      // store private key in browser local storage
+      // FIXME: Convert to encrypted PEM, store in localStorage
+      //localStorage.setItem(hash, JSON.stringify(keypair.privateKey));
+      // to retrieve the private key, do the following
+      // var privateKey = localStorage.getItem(hash)
+
+      // generate the DID and encrypted DID data
+      did = didio.generateDid();
+      return new Promise(function(resolve, reject) {
+        didio.encrypt(did, self.passphrase, function(err, encryptedDid) {
+          self.generating = false;
+          if(err) {
+            return reject(err);
+          }
+          resolve(encryptedDid);
+        });
+      });
+    }).then(function(encryptedDid) {
+      // store the hash to encryptedDid mapping
+      var hash = didio.generateHash(self.username, self.passphrase);
+      var mappingData = {
+        '@context': 'https://w3id.org/identity/v1',
+        id: 'urn:sha256:' + hash,
+        cipherData: encryptedDid
+      };
+      return Promise.resolve($http.post('/mappings/', mappingData));
+    }).then(function(response) {
+      console.log("RES", response);
+      if(response.status !== 201) {
+        throw response;
+      }
+    }).then(function() {
+      // store the DID document
+      var didDocument = {
+        '@context': 'https://w3id.org/identity/v1',
+        id: did,
+        idp: idpInfo,
+        publicKeys: [forge.pki.publicKeyToPem(keypair.publicKey)]
+      };
+      return Promise.resolve($http.post('/dids/', didDocument))
+        .then(function(response) {
+          if(response.status !== 201) {
+            throw response;
+          }
+          DataService.redirect(DataService.get('idpInfo').url);
+        });
+    }).catch(function(err) {
+      console.error('Failed to register with the network', err);
+      brAlertService.add('error',
+        'Failed to register with the network. Try a different email ' +
+        'address and passphrase');
+      self.generating = false;
+      self.registering = false;
+    }).then(function() {
+      $scope.$apply();
+    });
   };
 });
 
