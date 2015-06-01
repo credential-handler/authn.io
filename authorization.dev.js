d48a6334b81b0617b0689bb3181a8b6a7cc6d143
Manu Sporny <msporny@digitalbazaar.com>
2015-06-01 12:09:40 -0400

    Move dev configuration to separate config file.

--- authorization.dev.js
+++ authorization.dev.js
@@ -8,24 +8,7 @@ var config = require('bedrock').config;
 var path = require('path');
 
 require('./lib/authorizationio');
-
-config.views.paths.push(path.join(__dirname, 'tests/views'));
-
-// load the development-specific extensions to the site
-require('./tests/lib/idp');
-require('./tests/lib/consumer');
-// pseudo bower package for idp and consumer
-config.requirejs.bower.packages.push({
-  path: path.join(__dirname, 'tests/components'),
-  manifest: {
-    name: 'idp-components',
-    moduleType: 'amd',
-    main: './main.js',
-    dependencies: {
-      angular: '~1.3.0'
-    }
-  }
-});
+require('./configs/dev');
 
 // configure for tests
 bedrock.events.on('bedrock.test.configure', function() {
---
+++ configs/dev.js
@@ -0,0 +1,28 @@
+/*
+ * authorization.io test configuration.
+ *
+ * Copyright (c) 2015 The Open Payments Foundation. All rights reserved.
+ */
+var config = require('bedrock').config;
+var path = require('path');
+
+config.views.paths.push(path.join(__dirname, '..', 'tests', 'views'));
+
+// load the development-specific extensions to the site
+require('../tests/lib/idp');
+require('../tests/lib/consumer');
+// pseudo bower package for idp and consumer
+config.requirejs.bower.packages.push({
+  path: path.join(__dirname, '..', 'tests', 'components'),
+  manifest: {
+    name: 'idp-components',
+    moduleType: 'amd',
+    main: './main.js',
+    dependencies: {
+      angular: '~1.3.0'
+    }
+  }
+});
+
+// bootstrap the Angular application so routes work
+config.views.routes.push(['/idp', 'index.html']);
