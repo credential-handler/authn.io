c1579db42769362a2f4bf97c0f5159d0713989f5
Matthew Collier <collier.matthew@gmail.com>
2015-06-03 16:56:00 -0400

    Validate and test identity registration form.

--- tests/components/idp/register.js
+++ tests/components/idp/register.js
@@ -2,8 +2,10 @@ define([
   'angular',
   'forge/forge',
   'did-io',
-  'node-uuid'
-], function(angular, forge, didiojs, uuid) {
+  'node-uuid',
+  './register-controller',
+  './register-directive'
+], function(angular, forge, didiojs, uuid, registerController, registerDirective) {
 
 'use strict';
 
@@ -13,126 +15,7 @@ var didio = didiojs({inject: {
   uuid: uuid
 }});
 
-module.controller('RegisterController', function(
-  $scope, $http, $window, config, DataService, brAlertService) {
-  var self = this;
-
-  if(config.data.idp) {
-    DataService.set('idp', config.data.idp);
-  }
-  if(config.data.registrationCallback) {
-    DataService.set('callback', config.data.registrationCallback);
-  }
-
-  self.passphraseConfirmation = '';
-  self.passphrase = '';
-  self.username = '';
-  self.registering = false;
-  self.generating = false;
-
-  if(!DataService.get('idp')) {
-    DataService.redirect('/register/idp-error');
-  }
-
-  self.register = function() {
-    // TODO: Add more validation checks
-    if(self.passphrase != self.passphraseConfirmation) {
-      return brAlertService.add('error',
-        'The passphrases you entered do not match.');
-    }
-    if(self.username.length == 0) {
-      return brAlertService.add('error',
-        'You failed to provide an email address');
-    }
-    var idp = DataService.get('idp');
-
-    // generate the private key
-    self.generating = true;
-    var pki = forge.pki;
-    var keypair = null;
-    var did = null;
-    var hash = didio.generateHash(self.username, self.passphrase);
-
-    new Promise(function(resolve, reject) {
-      self.generating = true;
-      pki.rsa.generateKeyPair({
-        bits: 2048,
-        workerScript: '/bower-components/forge/js/prime.worker.js'
-      }, function(err, keypair) {
-        if(err) {
-          return reject(err);
-        }
-        return resolve(keypair);
-      });
-    }).then(function(kp) {
-      keypair = kp;
-      // store encrypted private key in browser local storage
-      var encryptedPem =  pki.encryptRsaPrivateKey(
-        keypair.privateKey, self.username + self.passphrase);
-      localStorage.setItem(hash, encryptedPem);
-      self.generating = false;
-
-      // generate the DID
-      did = didio.generateDid();
-
-      // store the hash to did mapping
-      var mappingData = {
-        '@context': 'https://w3id.org/identity/v1',
-        id: hash,
-        did: did,
-        accessControl: {
-          writePermission: [{
-            id: did + '/keys/1',
-            type: 'CryptographicKey'
-          }]
-        }
-      };
-
-      return Promise.resolve($http.post('/mappings/', mappingData));
-    }).then(function(response) {
-      if(response.status !== 201) {
-        throw response;
-      }
-    }).then(function() {
-      // create the DID document
-      var didDocument = {
-        '@context': 'https://w3id.org/identity/v1',
-        id: did,
-        idp: idp,
-        accessControl: {
-          writePermission: [{
-            id: did + '/keys/1',
-            type: 'CryptographicKey'
-          }, {
-            id: idp,
-            type: 'Identity'
-          }],
-        },
-        publicKey: [{
-          id: did + '/keys/1',
-          type: 'CryptographicKey',
-          owner: did,
-          publicKeyPem: forge.pki.publicKeyToPem(keypair.publicKey)
-        }]
-      };
-      return Promise.resolve($http.post('/dids/', didDocument))
-        .then(function(response) {
-          if(response.status !== 201) {
-            throw response;
-          }
-          DataService.redirect(DataService.getUrl('idp'));
-        });
-    }).catch(function(err) {
-      console.error('Failed to register with the network', err);
-      brAlertService.add('error',
-        'Failed to register with the network. Try a different email ' +
-        'address and passphrase');
-      self.generating = false;
-      self.registering = false;
-    }).then(function() {
-      $scope.$apply();
-    });
-  };
-});
+module.controller(registerController);
+module.directive(registerDirective);
 
 });
--- tests/protractor/tests/e2e.js
+++ tests/protractor/tests/e2e.js
@@ -2,14 +2,66 @@ var bedrock = GLOBAL.bedrock;
 
 describe('registration', function() {
   var baseId = bedrock.randomString().toLowerCase();
-
+  var threeCharacters = 'abc';
+  var oneHundredSixtyCharacters =
+    'yA2NdBthMcnTqGYz3Eqe9uNHxM8u00TaooiuhIM' +
+    'P45C2nfqXTN17c1QubT0szamTrfACOBOvNs5m67' +
+    'yA2NdBthMcnTqGYz3Eqe9uNHxM8u00TaooiuhIM' +
+    'P45C2nfqXTN17c1QubT0szamTrfACOBOvNs5m67';
   var identity = {};
   identity.email = baseId + '@authorization.dev';
   identity.passphrase = 'ThisIsALongPassphrase23';
 
-  it('should create a mapping and DID document', function() {
-    // Override default timeout, RSA key generation is slow on older CPUs.
-    this.timeout(120000);    
-    bedrock.pages.idp.registerDid(identity);
+  describe('test identity registration form', function() {
+
+    beforeEach(function(){
+      bedrock.pages.idp.navigateToRegistrationForm()
+    });
+
+    it('should contain the proper form elements', function() {
+      bedrock.pages.idp.checkFields();
+    });
+
+    it('should warn on empty email', function() {
+      bedrock.pages.idp.testField('model.username', '', 'required');
+    });
+
+    it('should warn on short email', function() {
+      bedrock.pages.idp.testField(
+        'model.username', threeCharacters, 'minlength');
+    });
+
+    it('should warn on long email', function() {
+      bedrock.pages.idp.testField(
+        'model.username', oneHundredSixtyCharacters, 'maxlength');
+    });
+
+    it('should warn on empty passphrase', function() {
+      bedrock.pages.idp.testField('model.passphrase', '', 'required');
+    });
+
+    it('should warn on short passphrase', function() {
+      bedrock.pages.idp.testField(
+        'model.passphrase', threeCharacters, 'minlength');
+    });
+
+    it('should warn on long passphrase', function() {
+      bedrock.pages.idp.testField(
+        'model.passphrase', oneHundredSixtyCharacters, 'maxlength');
+    });
+
+    it('should warn if passphrase and confirmation do not match', function() {
+      bedrock.pages.idp.testFieldsMatch(
+        'model.passphrase', 'model.passphraseConfirmation', 'goodPhraseA',
+        'nonMatchingPhraseB', 'inputMatch');
+    });
+
+    it('should create a mapping and DID document', function() {
+      // Override default timeout, RSA key generation is slow on older CPUs.
+      this.timeout(180000);
+      bedrock.pages.idp.registerDid(identity);
+    });
+
   });
+
 });
