7f5168739dc20f29b5af6f21b513cacb42f2f19d
Manu Sporny <msporny@digitalbazaar.com>
2015-06-25 17:11:04 -0400

    Updated registration UI to use proof of patience.

--- tests/components/idp/register-controller.js
+++ tests/components/idp/register-controller.js
@@ -17,6 +17,64 @@ function factory($scope, $http, $location, ipCookie, brAlertService) {
   self.generating = false;
 
   /**
+   * Helper method to generate a keypair asynchronously.
+   *
+   * @return a promise that resolves to a keypair, or rejects with the error.
+   */
+  self._generateKeyPair = function() {
+    return new Promise(function(resolve, reject) {
+      forge.pki.rsa.generateKeyPair({
+        bits: 2048,
+        workerScript: '/bower-components/forge/js/prime.worker.js'
+      }, function(err, keypair) {
+        if(err) {
+          return reject(err);
+        }
+        return resolve(keypair);
+      });
+    });
+  }
+
+  /**
+   * Helper function to establish a proof of patience for writing a
+   * DID document.
+   *
+   * @return a promise that resolves to the proof, or rejects with the error.
+   */
+  self._establishProofOfPatience = function(didDocument) {
+    return new Promise(function(resolve, reject) {
+      self.registering = true;
+      return Promise.resolve($http.post('/dids/', didDocument, {
+        transformResponse: function(data, headers, status) {
+          return {
+            status: status,
+            headers: {
+              'retry-after': parseInt(headers('retry-after'), 10),
+              'www-authenticate': headers('www-authenticate')
+            },
+            data: data
+          };
+        }})).then(function(response) {
+          // expecting a 401 HTTP error code, so a success of any kind is bad
+          return reject(response);
+        }).catch(function(err) {
+          // expect 401 unauthorized and a proof-of-patience challenge
+          if(err.status !== 401) {
+            return reject(err);
+          }
+
+          // wait for as long as the proof of patience requires
+          self.secondsLeft = err.headers['retry-after'];
+          var waitTime = self.secondsLeft * 1000;
+          setTimeout(function() {
+            var proof = err.headers['www-authenticate'];
+            return resolve(proof);
+          }, waitTime);
+        });
+    });
+  }
+
+  /**
    * Validates the form, and if valid, performs a registration
    */
   self.validateForm = function() {
@@ -34,10 +92,14 @@ function factory($scope, $http, $location, ipCookie, brAlertService) {
     var keypair = null;
     var did = null;
     var hash = didio.generateHash(self.username, self.passphrase);
+    var mappingData = {};
+    var didDocument = {};
 
     self.registering = true;
     self.generating = true;
-    generateKeyPair().then(function(kp) {
+    self.secondsLeft = 0;
+
+    self._generateKeyPair().then(function(kp) {
       keypair = kp;
       // store encrypted private key in browser local storage
       var encryptedPem = forge.pki.encryptRsaPrivateKey(
@@ -48,27 +110,8 @@ function factory($scope, $http, $location, ipCookie, brAlertService) {
       // generate the DID
       did = didio.generateDid();
 
-      // store the hash to did mapping
-      var mappingData = {
-        '@context': 'https://w3id.org/identity/v1',
-        id: hash,
-        did: did,
-        accessControl: {
-          writePermission: [{
-            id: did + '/keys/1',
-            type: 'CryptographicKey'
-          }]
-        }
-      };
-
-      return Promise.resolve($http.post('/mappings/', mappingData));
-    }).then(function(response) {
-      if(response.status !== 201) {
-        throw response;
-      }
-    }).then(function() {
       // create the DID document
-      var didDocument = {
+      didDocument = {
         '@context': 'https://w3id.org/identity/v1',
         id: did,
         idp: idp,
@@ -88,12 +131,42 @@ function factory($scope, $http, $location, ipCookie, brAlertService) {
           publicKeyPem: forge.pki.publicKeyToPem(keypair.publicKey)
         }]
       };
-      return Promise.resolve($http.post('/dids/', didDocument))
-        .then(function(response) {
-          if(response.status !== 201) {
-            throw response;
-          }
-        });
+
+      // create the mapping document
+      mappingData = {
+        '@context': 'https://w3id.org/identity/v1',
+        id: hash,
+        did: did,
+        accessControl: {
+          writePermission: [{
+            id: did + '/keys/1',
+            type: 'CryptographicKey'
+          }]
+        }
+      };
+
+      // wait until the proof of patience has been established
+      return self._establishProofOfPatience(didDocument);
+    }).then(function(proof) {
+      console.log("ProofOfPatience", proof);
+      // use the proof of patience to register the DID
+      return Promise.resolve($http.post('/dids/', didDocument, {
+        headers: {
+          authorization: proof
+        }
+      })).then(function(response) {
+        if(response.status !== 201) {
+          throw response;
+        }
+      });
+    }).then(function(response) {
+      // register the mapping document
+      return Promise.resolve($http.post('/mappings/', mappingData));
+    }).then(function(response) {
+      console.log("Mapping created", response);
+      if(response.status !== 201) {
+        throw response;
+      }
     }).then(function() {
       ipCookie('did', did);
       var emailCredential = {
@@ -141,19 +214,5 @@ function factory($scope, $http, $location, ipCookie, brAlertService) {
 }
 
 return {RegisterController: factory};
-
-function generateKeyPair() {
-  return new Promise(function(resolve, reject) {
-    forge.pki.rsa.generateKeyPair({
-      bits: 2048,
-      workerScript: '/bower-components/forge/js/prime.worker.js'
-    }, function(err, keypair) {
-      if(err) {
-        return reject(err);
-      }
-      return resolve(keypair);
-    });
-  });
-}
 
 });
