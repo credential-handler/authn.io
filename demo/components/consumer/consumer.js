ead2d4d935c1542be62ca3aafb6246eb8be98671
Dave Longley <dlongley@digitalbazaar.com>
2015-10-06 00:08:09 -0400

    Fix consumer link and controller registration.

--- demo/components/consumer/consumer.js
+++ demo/components/consumer/consumer.js
@@ -1,8 +1,13 @@
-define(['angular'], function(angular) {
+define([
+  'angular',
+  './consumer-controller'
+], function(angular, consumerController) {
 
 'use strict';
 
 var module = angular.module('authio-demo.consumer', ['bedrock.alert']);
+
+module.controller(consumerController);
 
 /* @ngInject */
 module.config(function($routeProvider) {
