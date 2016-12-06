e741be4c2d8f9c5078e8fcc9acffb09e697545d6
Matthew Collier <collier.matthew@gmail.com>
2016-12-06 13:36:30 -0500

    Add demo config.

---
+++ demo.authorization.io.js
@@ -0,0 +1,16 @@
+/*!
+ * authorization.io production server.
+ *
+ * New BSD License (3-clause)
+ * Copyright (c) 2015-2016, Digital Bazaar, Inc.
+ * Copyright (c) 2015-2016, Accreditrust Technologies, LLC
+ * All rights reserved.
+ */
+var bedrock = require('bedrock');
+var config = require('bedrock').config;
+var path = require('path');
+
+require('./lib/index');
+require('./configs/demo.authorization.io');
+
+bedrock.start();
