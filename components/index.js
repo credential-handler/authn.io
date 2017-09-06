64dd13afd4861ad03da22abd9a9ae72c95fa368a
Dave Longley <dlongley@digitalbazaar.com>
2017-09-06 16:16:47 -0400

    Remove legacy support.

--- components/index.js
+++ components/index.js
@@ -8,16 +8,10 @@ import * as bedrock from 'bedrock-angular';
 import HandlerWindowHeaderComponent from './handler-window-header-component.js';
 import HomeComponent from './home-component.js';
 import MediatorComponent from './mediator-component.js';
-import './legacy/index.js';
 
 'use strict';
 
 const deps = ['web-request-mediator'];
-
-// only include legacy module when not loading new routes
-if(!['/', '/mediator'].includes(window.location.pathname)) {
-  deps.push('authio.legacy');
-}
 
 const module = angular.module('authio', deps);
 module.component('aioHandlerWindowHeader', HandlerWindowHeaderComponent);
