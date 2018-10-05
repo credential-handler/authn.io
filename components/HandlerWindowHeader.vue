16f93cd6865b372a56371bae90484296e96afe0b
Dave Longley <dlongley@digitalbazaar.com>
2018-10-04 21:38:53 -0400

    Adjust font sizes.

--- components/HandlerWindowHeader.vue
+++ components/HandlerWindowHeader.vue
@@ -4,14 +4,16 @@
       class="wrm-flex-row wrm-header-hint"
       style="padding: 5px; margin: 1px 0;">
       <i v-if="!hint.icon"
-        class="fa fa-user-circle fa-3x wrm-flex-item"
-        style="padding-left: 8px"></i>
+        class="fa fa-user-circle wrm-flex-item"
+        style="padding-left: 8px; font-size: 48px"></i>
       <img v-if="hint.icon" :src="hint.icon.fetchedImage"
-        style="width: 40px; max-height: 40px" class="wrm-flex-item">
-      <div style="margin-left: 10px;" class="wrm-flex-item-grow">
+        style="width: 48px; max-height: 48px" class="wrm-flex-item">
+      <div
+        style="margin-left: 10px; font-size: 14px"
+        class="wrm-flex-item-grow">
         You are using <strong>{{hint.name}}</strong> to
         <span v-if="operation === 'store'">store credentials</span>
-        <span v-else>provide credentials</span>
+        <span v-else>share credentials</span>
         <h6>
           <i class="fa fa-lock wrm-flex-item wrm-green"></i>
           <span class="wrm-green">https</span>://{{hint.origin}}
