bdeed7ed5d59b55214233db7639e39a26301ad1b
Dave Longley <dlongley@digitalbazaar.com>
2018-10-09 17:05:57 -0400

    Add ellipsis overflow CSS.

--- components/HandlerWindowHeader.vue
+++ components/HandlerWindowHeader.vue
@@ -2,7 +2,7 @@
   <div class="wrm-panel" style="height: 60px">
     <div
       class="wrm-flex-row wrm-header-hint"
-      style="padding: 5px; margin: 1px 0;">
+      style="padding: 5px; margin: 1px 0">
       <i v-if="!hint.icon"
         class="fas fa-wallet wrm-flex-item"
         style="padding-left: 8px; font-size: 48px"></i>
@@ -10,11 +10,11 @@
         style="width: 48px; max-height: 48px" class="wrm-flex-item">
       <div
         style="margin-left: 10px; font-size: 14px"
-        class="wrm-flex-item-grow">
+        class="wrm-flex-item-grow wrm-ellipsis">
         You are using <strong>{{hint.name}}</strong> to
         <span v-if="operation === 'store'">store credentials</span>
         <span v-else>share credentials</span>
-        <h6>
+        <h6 class="wrm-ellipsis">
           <i class="fas fa-lock wrm-flex-item wrm-green"></i>
           <span class="wrm-green">https</span>://{{hint.origin.substr(8)}}
         </h6>
