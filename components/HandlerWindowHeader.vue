08b0a53a34a6fce53e5566d7d24c9b141c41b77a
Dave Longley <dlongley@digitalbazaar.com>
2021-05-05 09:29:33 -0400

    Remove unnecessary "use strict" from ESM scripts.

--- components/HandlerWindowHeader.vue
+++ components/HandlerWindowHeader.vue
@@ -23,11 +23,9 @@
 <script>
 /*!
  * New BSD License (3-clause)
- * Copyright (c) 2017-2018, Digital Bazaar, Inc.
+ * Copyright (c) 2017-2021, Digital Bazaar, Inc.
  * All rights reserved.
  */
-'use strict';
-
 export default {
   name: 'HandlerWindowHeader',
   props: {
--- components/MediatorGreeting.vue
+++ components/MediatorGreeting.vue
@@ -17,11 +17,9 @@
 <script>
 /*!
  * New BSD License (3-clause)
- * Copyright (c) 2019, Digital Bazaar, Inc.
+ * Copyright (c) 2019-2021, Digital Bazaar, Inc.
  * All rights reserved.
  */
-'use strict';
-
 export default {
   name: 'MediatorGreeting',
   components: {},
