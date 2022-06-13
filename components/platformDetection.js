0e29075cb622f354a1a6e4968d0ffa21b18f314e
Dave Longley <dlongley@digitalbazaar.com>
2022-06-13 11:49:33 -0400

    Improve platform detection.

--- components/platformDetection.js
+++ components/platformDetection.js
@@ -1,11 +1,17 @@
+/*!
+ * New BSD License (3-clause)
+ * Copyright (c) 2017-2022, Digital Bazaar, Inc.
+ * All rights reserved.
+ */
 import {detect} from 'detect-browser';
 
 const browser = detect();
 
-export function shouldUseFirstPartyMode() {
-  if(browser && browser.name === 'chrome') {
-    return false;
-  }
+// always use 1p window if browser supports Storage Access API (as it
+// implies partitioned 3rd party storage) or if browser is not chrome
+const _useFirstPartyMode = document.requestStorageAccess || navigator.brave ||
+  !(browser && browser.name === 'chrome');
 
-  return true;
+export function shouldUseFirstPartyMode() {
+  return _useFirstPartyMode;
 }
