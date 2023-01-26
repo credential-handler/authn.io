b805bc1574adfd5954cf08221123346ea31c5aa3
Dave Longley <dlongley@digitalbazaar.com>
2023-01-26 14:13:50 -0500

    Update 1p/3p platform detection.

--- components/platformDetection.js
+++ components/platformDetection.js
@@ -1,6 +1,6 @@
 /*!
  * New BSD License (3-clause)
- * Copyright (c) 2017-2022, Digital Bazaar, Inc.
+ * Copyright (c) 2017-2023, Digital Bazaar, Inc.
  * All rights reserved.
  */
 import {detect} from 'detect-browser';
@@ -14,11 +14,21 @@ try {
   hasLocalStorage = false;
 }
 
-// always use 1p window if browser supports Storage Access API (as it
-// implies partitioned 3rd party storage), is brave, has no `localStorage`,
-// or if browser is not chrome
-const _useFirstPartyMode = document.requestStorageAccess || navigator.brave ||
-  !hasLocalStorage || !(browser && browser.name === 'chrome');
+// specifically, branded Google Chrome, not just chromium
+let isChrome;
+try {
+  isChrome = browser && browser.name === 'chrome' &&
+    navigator.userAgentData &&
+    navigator.userAgentData.brands.some(({brand}) => brand === 'Google Chrome');
+} catch(e) {
+  isChrome = false;
+}
+
+// always use 1p window if browser:
+// 1. is Brave
+// 2. is not Google Chrome
+// 3. has no local storage
+const _useFirstPartyMode = !!(navigator.brave || !isChrome || !hasLocalStorage);
 
 export function shouldUseFirstPartyMode() {
   return _useFirstPartyMode;
