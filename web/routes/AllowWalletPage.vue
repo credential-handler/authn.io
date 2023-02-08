3211f945d1ddd2f7abd26655d18ef6296bf48f18
Dave Longley <dlongley@digitalbazaar.com>
2023-02-08 16:08:04 -0500

    Create separate route files.

---
+++ web/routes/AllowWalletPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <AllowWalletDialog />
+</template>
+
+<script>
+/*!
+ * New BSD License (3-clause)
+ * Copyright (c) 2017-2023, Digital Bazaar, Inc.
+ * All rights reserved.
+ */
+import AllowWalletDialog from '../components/AllowWalletDialog.vue';
+
+export default {
+  name: 'AllowWalletPage',
+  components: {AllowWalletDialog}
+};
+</script>
+
+<style>
+</style>
---
+++ web/routes/MediatorPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <Mediator />
+</template>
+
+<script>
+/*!
+ * New BSD License (3-clause)
+ * Copyright (c) 2017-2023, Digital Bazaar, Inc.
+ * All rights reserved.
+ */
+import Mediator from '../components/Mediator.vue';
+
+export default {
+  name: 'MediatorPage',
+  components: {Mediator}
+};
+</script>
+
+<style>
+</style>
