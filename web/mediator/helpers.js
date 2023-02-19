45b90fd48463daf2b918e491fe6b67085b3de665
Dave Longley <dlongley@digitalbazaar.com>
2023-02-19 18:16:53 -0500

    Consoldate default hint option creation code.

--- web/mediator/helpers.js
+++ web/mediator/helpers.js
@@ -24,28 +24,3 @@ export async function autoRegisterHint({hint}) {
   await navigator.credentialMediator.ui.registerCredentialHandler(
     credentialHandler, {name, enabledTypes, icons: []});
 }
-
-// FIXME: change to `createRegistrationHintOption` or similar
-export async function createDefaultHintOption({origin, manifest} = {}) {
-  if(!(manifest && manifest.credential_handler &&
-    manifest.credential_handler.url &&
-    Array.isArray(manifest.credential_handler.enabledTypes))) {
-    // manifest does not have credential handler info
-    return null;
-  }
-
-  // resolve credential handler URL
-  let credentialHandler;
-  try {
-    credentialHandler = new URL(manifest.credential_handler.url, origin).href;
-  } catch(e) {
-    console.error(e);
-    return null;
-  }
-
-  return {
-    credentialHandler,
-    credentialHintKey: 'default',
-    enabledTypes: manifest.credential_handler.enabledTypes
-  };
-}
