475df257477be0886de2d5f80478e52d71d3f877
Dave Longley <dlongley@digitalbazaar.com>
2023-02-22 14:26:51 -0500

    Update router to Vue 3.

--- web/router.js
+++ web/router.js
@@ -1,44 +1,35 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
-import VueRouter from 'vue-router';
+import {createRouter as _createRouter, createWebHistory} from 'vue-router';
 
 export async function createRouter() {
-  const router = new VueRouter({
-    mode: 'history',
+  return _createRouter({
+    mode: createWebHistory(),
     routes: [{
       path: '/',
       component: () => import(
         /* webpackChunkName: "LandingPage" */
         './routes/LandingPage.vue'),
-      meta: {
-        title: 'authn.io'
-      }
+      meta: {title: 'authn.io'}
     }, {
       path: '/mediator',
       component: () => import(
         /* webpackChunkName: "ThirdPartyMediatorPage" */
         './routes/ThirdPartyMediatorPage.vue'),
-      meta: {
-        title: 'Credential Mediator'
-      }
+      meta: {title: 'Credential Mediator'}
     }, {
       path: '/mediator/wallet-chooser',
       component: () => import(
         /* webpackChunkName: "FirstPartyMediatorPage" */
         './routes/FirstPartyMediatorPage.vue'),
-      meta: {
-        title: 'Credential Wallet Chooser'
-      }
+      meta: {title: 'Credential Wallet Chooser'}
     }, {
       path: '/mediator/allow-wallet',
       component: () => import(
         /* webpackChunkName: "FirstPartyMediatorPage" */
         './routes/FirstPartyMediatorPage.vue'),
-      meta: {
-        title: 'Allow Wallet'
-      }
+      meta: {title: 'Allow Wallet'}
     }]
   });
-  return router;
 }
