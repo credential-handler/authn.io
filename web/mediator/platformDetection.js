44cdaa80348ca3e314209b602216ce5da209480d
Dave Longley <dlongley@digitalbazaar.com>
2023-11-02 16:48:59 -0400

    Always assume every browser has partitioned storage.

--- web/mediator/platformDetection.js
+++ web/mediator/platformDetection.js
@@ -3,32 +3,7 @@
  * Copyright (c) 2017-2023, Digital Bazaar, Inc.
  * All rights reserved.
  */
-import {detect} from 'detect-browser';
-
-const browser = detect();
-
-let hasLocalStorage;
-try {
-  hasLocalStorage = !!localStorage;
-} catch(e) {
-  hasLocalStorage = false;
-}
-
-// specifically, branded Google Chrome, not just chromium
-let isChrome;
-try {
-  isChrome = browser?.name === 'chrome' &&
-  navigator.userAgentData?.brands.some(({brand}) => brand === 'Google Chrome');
-} catch(e) {
-  isChrome = false;
-}
-
-// platform has partitioned storage if browser:
-// 1. is Brave
-// 2. is not Google Chrome
-// 3. has no local storage
-const _partitioned = !!(navigator.brave || !isChrome || !hasLocalStorage);
-
 export function hasPartitionedStorage() {
-  return _partitioned;
+  // partitioned storage is now always used for every platform
+  return true;
 }
