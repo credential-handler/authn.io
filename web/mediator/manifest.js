e9f05bed4df9c6d84898da41510ece4a96cb8911
James Easter <jeaster@digitalbazaar.com>
2024-02-02 14:10:22 -0700

    Refactor getWebAppManifest for better maintainability.

--- web/mediator/manifest.js
+++ web/mediator/manifest.js
@@ -20,13 +20,43 @@ export async function getWebAppManifest({origin, timeout = 1000}) {
 }
 
 async function _getWebAppManifest({origin}) {
-  let url;
-  let response;
+  // urls are in order of priority
   const urls = [
-    `${origin}/manifest.json`
-    `${origin}/manifest.webmanifest`,
+    `${origin}/manifest.json`,
+    `${origin}/manifest.webmanifest`
   ];
+  const responses = await Promise.allSettled(
+    urls.map(url => _fetchWithCache(url))
+  );
+  const response = responses.find(({value}) => value !== null);
+  return response.value || null;
+}
 
+async function _parseBody(response) {
+  if(!response.ok) {
+    throw new Error(
+      `Response: ${response.status} ${response.statusText}`);
+  }
+  if(!_isContentJson(response)) {
+    throw new Error('Content is not JSON.');
+  }
+  return response.json();
+}
+
+function _isContentJson(response) {
+  const {type} = contentType.parse(response.headers.get('content-type'));
+  return ['application/manifest+json', 'application/json'].includes(type);
+}
+
+async function _legacyFetch(url) {
+  const response = await httpClient.get(url, {credentials: 'omit'});
+  if(!_isContentJson(response)) {
+    throw new Error('Content is not JSON.');
+  }
+  return response.data;
+}
+
+async function _fetchWithCache(url) {
   try {
     if(!cacheStorage && typeof caches !== 'undefined') {
       try {
@@ -37,37 +67,26 @@ async function _getWebAppManifest({origin}) {
     }
     if(!cacheStorage) {
       // no cache storage/API available, fetch directly
-      return await _legacyFetch(urls);
+      return await _legacyFetch(url);
     }
 
-    // iterate through urls for cached response
-    for(let i = 0; i < urls.length; i++) {
-      const url = urls[i];
-      // try to get cached response
-      response = await cacheStorage.match(url);
-      if(response) {
-        const expires = new Date(response.headers.get('expires'));
-        const now = new Date();
-        if(expires >= now) {
-          // return cached response
-          return await _parseBody(response);
-        }
-        // remove expired response from cache
-        await cacheStorage.delete(url);
+    // try to get cached response
+    let response = await cacheStorage.match(url);
+    if(response) {
+      const expires = new Date(response.headers.get('expires'));
+      const now = new Date();
+      if(expires >= now) {
+        // return cached response
+        return await _parseBody(response);
       }
+      // remove expired response from cache
+      await cacheStorage.delete(url);
     }
 
     let fetchError;
     try {
-      // fetch live responses
-      const allResponses = await Promise.allSettled(
-        urls.map(url => fetch(url, {credentials: 'omit'}))
-      );
-      const successfulResponse = allResponses.find(({status, value}) => {
-        return status === 'fulfilled' && value.ok;
-      });
-      response = successfulResponse.value;
-      url = successfulResponse.value.url;
+      // fetch live response
+      response = await fetch(url, {credentials: 'omit'});
 
       // build a cached response that will expire based on local config
       const headers = new Headers(response.headers);
@@ -121,34 +140,4 @@ async function _getWebAppManifest({origin}) {
       `Fixing this may improve the information displayed for "${origin}".`, e);
     return null;
   }
-}
-
-async function _parseBody(response) {
-  if(!response.ok) {
-    throw new Error(
-      `Response: ${response.status} ${response.statusText}`);
-  }
-  if(!_isContentJson(response)) {
-    throw new Error('Content is not JSON.');
-  }
-  return response.json();
-}
-
-function _isContentJson(response) {
-  const {type} = contentType.parse(response.headers.get('content-type'));
-  return ['application/manifest+json', 'application/json'].includes(type);
-}
-
-async function _legacyFetch(urls) {
-  const allResponses = await Promise.allSettled(
-    urls.map(url => httpClient.get(url, {credentials: 'omit'}))
-  );
-  const successfulResponse = allResponses.find(({status, value}) => {
-    return status === 'fulfilled' && value.ok;
-  });
-  const response = successfulResponse.value;
-  if(!_isContentJson(response)) {
-    throw new Error('Content is not JSON.');
-  }
-  return response.data;
 }
