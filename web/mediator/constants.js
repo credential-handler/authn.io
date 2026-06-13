9de39ec63bd534fcbdd408c5b87508e070257e55
Dave Longley <dlongley@digitalbazaar.com>
2026-06-13 16:26:02 -0400

    Make adding URL bar height to popup conditional.

--- web/mediator/constants.js
+++ web/mediator/constants.js
@@ -2,15 +2,19 @@
  * New BSD License (3-clause)
  * Copyright (c) 2017-2026, Digital Bazaar, Inc.
  */
+const addUrlBarHeight = navigator.userAgent.toLowerCase().includes('firefox');
+const urlBarHeight = addUrlBarHeight ? 30 : 0;
+
 export const DEFAULT_ALLOW_WALLET_POPUP_WIDTH = 500;
 // tall enough for the Block/Allow footer on browsers with tall popup
 // chrome (e.g. Firefox always shows a URL bar in popups)
-export const DEFAULT_ALLOW_WALLET_POPUP_HEIGHT = 260;
+export const DEFAULT_ALLOW_WALLET_POPUP_HEIGHT = 230 + urlBarHeight;
 
 export const DEFAULT_HANDLER_POPUP_WIDTH = 800;
 export const DEFAULT_HANDLER_POPUP_HEIGHT = 600;
 
 export const DEFAULT_HINT_CHOOSER_POPUP_WIDTH = 500;
-export const DEFAULT_HINT_CHOOSER_POPUP_HEIGHT = 440;
+export const DEFAULT_HINT_CHOOSER_POPUP_HEIGHT = 400 + urlBarHeight;
 // taller hint chooser popup that fits the cross-device QR section
-export const DEFAULT_CROSS_DEVICE_HINT_CHOOSER_POPUP_HEIGHT = 560;
+export const DEFAULT_CROSS_DEVICE_HINT_CHOOSER_POPUP_HEIGHT =
+  550 + urlBarHeight;
